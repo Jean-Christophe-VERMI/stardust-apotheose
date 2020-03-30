4e037a3d33c51f4c3d6db0e1a033c0e0f0e9f8b1
Jean-Christophe-VERMI <56582665+Jean-Christophe-VERMI@users.noreply.github.com>
2020-03-30 19:04:32 +0200

    ajout de NewThreads dans Forum

--- client/src/components/Forum/index.js
+++ client/src/components/Forum/index.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 // import { getUrlByThreadTitle } from 'src/selectors';
-
+import NewThread from 'src/containers/NewThread';
 import Threads from 'src/containers/Threads';
 import ThreadWithComments from 'src/components/ThreadWithComments';
 

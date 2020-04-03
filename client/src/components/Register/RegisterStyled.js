9cca8f19f968f8cca68b25c9bc20cfcaa8322796
ChloeJost <chloe.jost@live.fr>
2020-04-03 14:49:02 +0200

    captcha repare

--- client/src/components/Register/RegisterStyled.js
+++ client/src/components/Register/RegisterStyled.js
@@ -1,12 +1,9 @@
 import styled from 'styled-components';
 import stars from './images/register-bg.jpg';
-
 export const RegisterStyled = styled.div`
-
     background-image: url(${stars});
     background-position: center;
     background-size: cover;
-   
    #register-form {
     background-color: rgba(0, 0, 0, 0.3);
     display: flex;
@@ -14,59 +11,45 @@ export const RegisterStyled = styled.div`
     flex-direction: column;
     min-height: 100vh;
   }
-
   .register-form-element {
     background-color: rgba(130, 204, 221, 0.7);
-    padding: ${props => props.theme.sizings.large};
-    border-radius: ${props => props.theme.radius.medium};
+    padding: 2rem;
+    border-radius: 5px;
     margin-top: auto;
     margin-bottom: auto;
-
     h1 {
-      color: ${props => props.theme.colors.white};
+      color: white;
       text-align: center;
-      margin-bottom: ${props => props.theme.sizings.large};
-      font-size: ${props => props.theme.fontSizes.large};
+      margin-bottom: 2rem;
+      font-size: 1.5rem;
     }
-
     p{
-      color: ${props => props.theme.colors.white};
+      color: white;
       text-align: center;
     }
-
   }
-
-  field {
-    display: block;
-    padding: ${props => props.theme.sizings.small};
-    font-size: ${props => props.theme.fontSizes.default};
-    border: 1px solid ${props => props.theme.colors.darkBlue};
-    border-radius: ${props => props.theme.radius.medium};
-    width: 100%;
-    margin-top: ${props => props.theme.sizings.largest};
-  }
-
   .terms-button-submit {
     text-align: center;
   }
-
- 
-
   .terms {
-    color: ${props => props.theme.colors.white};
+    color: #ffffff;
   }
-
-
+  .button-submit {
+    color: #888888;
+    text-align: center;
+    border-radius: 3px;
+    border : none;
+    padding : .5rem .5rem;
+    margin-top: 2rem;
+    background-color: #ffffff;
     &:hover:enabled {
-      background-color : #0a3d62; // LE FICHIER DE THEME !
+      background-color : ${props => props.theme.colors.darkBlue};
       color : white;
       transition-duration: .3s;
-     
     }
   }
   button:hover:disabled{
     background-color : white;
 }   
 `;
-
 export default RegisterStyled;
